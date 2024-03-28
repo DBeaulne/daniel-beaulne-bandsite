0dda017ba07120551fff68b86163ca50069b4d2c
DBeaulne <dbeaulne1@gmail.com>
2024-03-28 13:58:21 -0400

    continue work on comments section of bio page

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,1 +1,36 @@
 /* Index Page Javascript */
+// Object declaration
+let comment = {
+    name: "username", // User's Full Name
+    avatar_present: true,
+    avatar: "avatar-path", // path to user's avatar. Blank avatar's have placeholder color of Mercury
+    date: "mm/dd/year", // date of comment
+    comment: "comment-text", // comment text
+};
+
+let comments = [
+    {
+        name: "Victor Pinto",
+        avatar_present: false,
+        avatar: "avatar-path",
+        date: "11/02/2023",
+        comment:
+            "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+    },
+    {
+        name: "Christina Cabrera",
+        avatar_present: false,
+        avatar: "avatar-path",
+        date: "10/28/2023",
+        comment:
+            "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+    },
+    {
+        name: "Isaac Tadesse",
+        avatar_present: false,
+        avatar: "avatar-path",
+        date: "10/20/2023",
+        comment:
+            "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+    },
+];
